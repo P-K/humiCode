6167a994a863910d56a109a795282cfc9bbf1f7c
PK <preksha.kashyap@gmail.com>
2018-08-01 22:27:51 -0400

    added test cases, methods for reading and writing users to db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,53 @@
 class User < ApplicationRecord
+	# need these fields for employees 
 	validates :first_name, presence: true
 	validates :last_name, presence: true
 	validates :birthday, presence: true
 	validates :marital_status, presence: true
 	validates :social_insurance_number, presence: true
 	validates :hire_date, presence: true
-	
+
+	# create record of new employee
+	def create_user_entry(first_name, last_name, birthday, marital_status, social_insurance_number, hire_date)
+		u = User.create(first_name: first_name,
+						last_name: last_name,
+						birthday: birthday,
+						marital_status: marital_status,
+						social_insurance_number: social_insurance_number,
+						hire_date: hire_date)
+		u.save # save created user to database
+	end
+
+	### Read Methods ###
+
+	# read all employee records from db
+	def fetch_all_users()
+		users = User.all
+		return users
+	end
+
+	# find all users with a certain first name
+	def fetch_users_by_firstname(first_name)
+		users = User.find_by(first_name: 'first_name')
+	end
+
+	# find all users with a certain last name
+	def fetch_users_by_lastname(last_name)
+		users = User.find_by(last_name: 'last_name')
+	end
+
+	# find all users with a certain birthday
+	def fetch_users_by_birthday(birthday)
+		users = User.find_by(birthday: 'birthday')
+	end
+
+	# find user with a particular SIN
+	def fetch_users_by_SIN(social_insurance_number)
+		user = User.find_by(social_insurance_number: 'social_insurance_number')
+	end
+
+	# find all users with a particular hire date
+	def fetch_users_by_hire_date(hire_date)
+		user = User.find_by(hire_date: 'hire_date')
+	end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,44 +1,65 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ 
+	# Testing validation 
+	test "user shouldn't have an empty first name" do
+		users(:one).first_name = nil
+		assert_nil users(:one).first_name
+		assert_equal false, users(:one).valid?
+	end
 
-  test "user shouldn't have an empty first name" do
-  	users(:one).first_name = nil
-  	assert_nil users(:one).first_name
-  	assert_equal false, users(:one).valid?
-  end
+	test "user shouldn't have an empty last name" do
+		users(:one).last_name = nil
+		assert_nil users(:one).last_name
+		assert_equal false, users(:one).valid?
+	end
 
-  test "user shouldn't have an empty last name" do
-  	users(:one).last_name = nil
-  	assert_nil users(:one).last_name
-  	assert_equal false, users(:one).valid?
-  end
+	test "user shouldn't have an empty date of birth" do
+		users(:one).birthday = nil
+		assert_nil users(:one).birthday
+		assert_equal false, users(:one).valid?
+	end
 
-  test "user shouldn't have an empty date of birth" do
-  	users(:one).birthday = nil
-  	assert_nil users(:one).birthday
-  	assert_equal false, users(:one).valid?
-  end
+	test "user shouldn't have an empty marital status" do
+		users(:one).marital_status = nil
+		assert_nil users(:one).marital_status
+		assert_equal false, users(:one).valid?
+	end
 
-  test "user shouldn't have an empty marital status" do
-  	users(:one).marital_status = nil
-  	assert_nil users(:one).marital_status
-  	assert_equal false, users(:one).valid?
-  end
+	test "user shouldn't have an empty SIN" do
+		users(:one).social_insurance_number = nil
+		assert_nil users(:one).social_insurance_number
+		assert_equal false, users(:one).valid?
+	end
 
-  test "user shouldn't have an empty SIN" do
-  	users(:one).social_insurance_number = nil
-  	assert_nil users(:one).social_insurance_number
-  	assert_equal false, users(:one).valid?
-  end
+	test "user shouldn't have an empty hire date" do
+		users(:one).hire_date = nil
+		assert_nil users(:one).hire_date
+		assert_equal false, users(:one).valid?
+	end
 
-  test "user shouldn't have an empty hire date" do
-  	users(:one).hire_date = nil
-  	assert_nil users(:one).hire_date
-  	assert_equal false, users(:one).valid?
-  end
+	# Test create method
+	test "user shouldn't save without necessary info" do
+		employee = users(:bad_user)
+		user = User.new
+		assert_not user.create_user_entry(employee.first_name,
+									employee.last_name,
+									employee.birthday,
+									employee.marital_status,
+									employee.social_insurance_number,
+									employee.hire_date)
+	end
+
+	test "user should save without all info" do
+		employee = users(:two)
+		user = User.new
+		assert user.create_user_entry(employee.first_name,
+									employee.last_name,
+									employee.birthday,
+									employee.marital_status,
+									employee.social_insurance_number,
+									employee.hire_date)
+	end
 
 end
