3e8396538ab851dda5eee60e929c750070693179
PK <preksha.kashyap@gmail.com>
2018-08-03 21:24:18 -0400

    created role model + columns, added role id to user table

---
+++ db/migrate/20180803225559_add_role_columns.rb
@@ -0,0 +1,8 @@
+class AddRoleColumns < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :roles, :id, :integer
+  	add_column :roles, :name, :varchar
+  	add_column :roles, :editCompensation, :boolean, :default => false
+  	add_column :roles, :accessRecords, :boolean, :default => false
+  end
+end
---
+++ db/migrate/20180803234549_add_role_i_dto_user.rb
@@ -0,0 +1,5 @@
+class AddRoleIDtoUser < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :users, :roleID, :integer
+  end
+end
